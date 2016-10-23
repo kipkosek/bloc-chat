041c4c9c22ba8a885d8b5dde24993a67eceff7ee
Kip Kosek <kipkosek@gmail.com>
2016-10-23 16:40:14 -0400

    Checkpoint 4 List Messages: Completed (update)

--- dist/scripts/services/MessageService.js
+++ dist/scripts/services/MessageService.js
@@ -4,18 +4,12 @@
 
     var messageRef = firebase.database().ref().child("messages");
     var messages = $firebaseArray(messageRef);
-    // var roomId = messages.roomId;
-
-    // MessageService.all = messages;
 
     MessageService.getByRoomId = function(currentRoomId) {
         console.log(currentRoomId);
         return $firebaseArray(messageRef.orderByChild("roomId").equalTo(currentRoomId));
     };
 
-    // RoomService.addRoom = function(room) {
-    //   rooms.$add(room);
-    // };
     return MessageService;
   }
 
